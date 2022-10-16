93ba089fb9ac3b5f132ba0c9e0ce76d14a55b624
UnMaxDeJoie <maxait93@gmail.com>
2022-10-16 15:59:42 +0200

    suite du travail week end

--- app/blog.php
+++ app/blog.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+//test pour ne pas accéder à la page blog sans être connecté
+//$_SESSION['email'] = $_POST['email'];
+//if(!isset($_SESSION['email'])){
+//    header("Location:./index.php");
+//}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,9 +15,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-coucou :)
+    <div id="content">
+        <p>coucou :)</p>
+    </div>
 </body>
 </html>
